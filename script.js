efb5f4f74668ba0b38fe8236448457fca9f4d795
meenu thakur <137192082+meenukashyap@users.noreply.github.com>
2025-06-21 10:39:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var age=20
+function displayage()
+{
+    console.log(age)
+}
+console.log(displayage())
+function changeage()
+{
+    let age=30
+    console.log(age)
+}
+console.log(displayage())
+console.log(changeage())
